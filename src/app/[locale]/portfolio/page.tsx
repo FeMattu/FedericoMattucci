aacb20b75760b783ce803e3ff34f8ebd0c8f1c61
Federico Mattucci <mattucci.federico@gmail.com>
2025-04-17 17:12:01 +0200

    fixed linting error

--- src/app/[locale]/portfolio/page.tsx
+++ src/app/[locale]/portfolio/page.tsx
@@ -4,8 +4,6 @@ import Gallery from "@/components/Gallery";
 import { useTranslations } from "next-intl";
 
 export default function Portfolio() {
-    const t = useTranslations();
-
     return (
         <div className="flex w-full mx-auto min-h-screen justify-center px-6">
             <div className="flex flex-col items-center w-full">
