7425414353ec1e3225b1a98a8d13a1e5ac257a70
Federico Mattucci <mattucci.federico@gmail.com>
2025-07-18 23:58:43 -0700

    dynamic process NEXTAUTH_URL

--- auth.ts
+++ auth.ts
@@ -2,6 +2,10 @@ import NextAuth from "next-auth"
 import "next-auth/jwt"
 import Google from "next-auth/providers/google"
 
+if (!process.env.NEXTAUTH_URL && process.env.VERCEL_URL) {
+  process.env.NEXTAUTH_URL = `https://${process.env.VERCEL_URL}`;
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   debug: !!process.env.AUTH_DEBUG,
   secret: process.env.NEXTAUTH_SECRET,
